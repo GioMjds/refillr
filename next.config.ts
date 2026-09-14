dfd5906d99ba9757054ae31e6859885afba1115a
GioMjds <giomjds@gmail.com>
2026-09-14 11:50:51 +0800

    init commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '3000',
+      },
+      {
+        protocol: 'https',
+        hostname: 'googleusercontent.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ proxy.ts
@@ -0,0 +1,5 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+export default async function proxy(req: NextRequest) {
+  
+}
